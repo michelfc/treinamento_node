957654137b7f7fef2767d5ed6e808a89acca7544
Michel Castro <michel.castro.ter@estacio.br>
2020-10-09 15:29:53 -0300

    melhorias nas rotas

--- controllers/professor.controller.js
+++ controllers/professor.controller.js
@@ -3,7 +3,7 @@ const professorService = require("../services/professor.service");
 module.exports = (app) => {
 
     app.get("/professor", async (req, res) => res.send(await professorService.find({})));
-    app.get("/professor/:id", async (req, res) => res.send(await professorService.find({ _id: req.params.id })));
+    app.get("/professor/:id", async (req, res) => res.send(await professorService.findById(req.params.id)));
     app.post("/professor", async (req, res) => res.send(await professorService.create(req.body)));
     app.put("/professor", async (req, res) => res.send(await professorService.updateOne({ _id: req.body._id }, req.body)))
     app.delete("/professor/:id", async (req, res) => res.send(await professorService.deleteOne({ _id: req.params.id })));
--- controllers/usuario.controller.js
+++ controllers/usuario.controller.js
@@ -3,7 +3,7 @@ const usuarioService = require("../services/usuario.service");
 module.exports = (app) => {
 
     app.get("/usuario", async (req, res) => res.send(await usuarioService.find({})));
-    app.get("/usuario/:id", async (req, res) => res.send(await usuarioService.find({ _id: req.params.id })));
+    app.get("/usuario/:id", async (req, res) => res.send(await usuarioService.findById(req.params.id)));
     app.post("/usuario", async (req, res) => res.send(await usuarioService.create(req.body)));
     app.put("/usuario", async (req, res) => res.send(await usuarioService.updateOne({ _id: req.body._id }, req.body)))
     app.delete("/usuario/:id", async (req, res) => res.send(await usuarioService.deleteOne({ _id: req.params.id })));
